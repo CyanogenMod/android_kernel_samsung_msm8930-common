51f13f4057b538638c9643cd327adc53fa32976d
Arne Coucheron <arco68@gmail.com>
2013-06-25 05:02:06 +0200

    board-8930: pmic: Enable RTC write

--- arch/arm/mach-msm/board-8930-pmic.c
+++ arch/arm/mach-msm/board-8930-pmic.c
@@ -316,7 +316,7 @@ static struct pm8xxx_mpp_platform_data pm8xxx_mpp_pdata __devinitdata = {
 };
 
 static struct pm8xxx_rtc_platform_data pm8xxx_rtc_pdata __devinitdata = {
-	.rtc_write_enable	= false,
+	.rtc_write_enable	= true,
 	.rtc_alarm_powerup	= false,
 };
 
