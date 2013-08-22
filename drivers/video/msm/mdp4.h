1662412bc0b26c30990ee03430dc5841c0cd0669
Arne Coucheron <arco68@gmail.com>
2013-08-22 05:13:58 +0200

    msm: display: Cleanup premultiplied stuff
    * Leftovers from Samsung
    
    Change-Id: Ibdcc8c56b61ccc303b57b6370353fb10f75f6d4d

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -995,9 +995,6 @@ int mdp4_calc_blt_mdp_bw(struct msm_fb_data_type *mfd,
 int mdp4_overlay_mdp_perf_req(struct msm_fb_data_type *mfd,
 				struct mdp4_overlay_pipe *plist);
 void mdp4_overlay_mdp_perf_upd(struct msm_fb_data_type *mfd, int flag);
-int mdp4_update_base_blend(struct msm_fb_data_type *mfd,
-				struct mdp_blend_cfg *mdp_blend_cfg);
-u32 mdp4_get_mixer_num(u32 panel_type);
 int mdp4_overlay_reset(void);
 void mdp4_vg_csc_restore(void);
 void dump_underrun_pipe_info(void);
