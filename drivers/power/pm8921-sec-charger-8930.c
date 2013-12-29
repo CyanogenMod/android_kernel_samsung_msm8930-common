bc273b3fa165b6ac6500d70f8242c7b9d0ba45fd
f4k <tusemociones@gmail.com>
2013-12-29 15:17:46 -0300

    mach-msm: add USB fastcharge support.

--- drivers/power/pm8921-sec-charger-8930.c
+++ drivers/power/pm8921-sec-charger-8930.c
@@ -38,6 +38,10 @@
 #include <mach/msm_hsusb.h>
 #include <mach/msm8930-gpio.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #include <linux/battery/sec_charging_common_8930.h>
 #include <linux/i2c/tsu6721.h>
 
@@ -3305,7 +3309,39 @@ static void __pm8921_charger_vbus_draw(unsigned int mA)
 			i--;
 		if (i < 0)
 			i = 0;
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if (force_fast_charge == 1)
+			i = 14;
+		else if (force_fast_charge == 2) {
+			switch (fast_charge_level) {
+				case FAST_CHARGE_500:
+					i = 2;
+					break;
+				case FAST_CHARGE_700:
+					i = 4;
+					break;
+				case FAST_CHARGE_900:
+					i = 8;
+					break;
+				case FAST_CHARGE_1100:
+					i = 10;
+					break;
+				case FAST_CHARGE_1300:
+					i = 12;
+					break;
+				case FAST_CHARGE_1500:
+					i = 14;
+					break;
+				default:
+					break;
+			}
+		}
 		rc = pm_chg_iusbmax_set(the_chip, i);
+		pr_info("charge curent index => %d\n", i);
+#else
+		rc = pm_chg_iusbmax_set(the_chip, i);
+#endif
 		if (rc)
 			pr_err("unable to set iusb to %d rc = %d\n", i, rc);
 	}
--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -52,6 +52,10 @@
 #include <mach/msm_bus.h>
 #include <mach/rpm-regulator.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -1198,6 +1202,11 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 
 	if (motg->cur_power == mA)
 		return;
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge > 0)
+		mA = IDEV_ACA_CHG_MAX;
+#endif
 
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
 
