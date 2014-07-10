8b4f89ef1a9eccab83cc6e5f86fcf65f7f621489
Arne Coucheron <arco68@gmail.com>
2014-07-11 01:32:50 +0200

    Revert "msm: mdp: Change msmfb kworker to kthread"
    This reverts commit 9a619e36cbfdc60a739625580f7ecbcd28021902.
    
    Change-Id: I28a30970b56709ffe504a7fdba1d54d2107543ed

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -25,7 +25,6 @@
 #include <linux/fb.h>
 #include <linux/msm_mdp.h>
 #include <linux/init.h>
-#include <linux/kthread.h>
 #include <linux/ioport.h>
 #include <linux/device.h>
 #include <linux/dma-mapping.h>
@@ -128,7 +127,7 @@ static int msm_fb_mmap(struct fb_info *info, struct vm_area_struct * vma);
 static int mdp_bl_scale_config(struct msm_fb_data_type *mfd,
 						struct mdp_bl_scale_data *data);
 static void msm_fb_scale_bl(__u32 *bl_lvl);
-static int msm_fb_commit_thread(void *data);
+static void msm_fb_commit_wq_handler(struct work_struct *work);
 static int msm_fb_pan_idle(struct msm_fb_data_type *mfd);
 
 #ifdef MSM_FB_ENABLE_DBGFS
@@ -568,7 +567,6 @@ static int msm_fb_remove(struct platform_device *pdev)
 		del_timer(&mfd->msmfb_no_update_notify_timer);
 	complete(&mfd->msmfb_no_update_notify);
 	complete(&mfd->msmfb_update_notify);
-	kthread_stop(mfd->commit_thread);
 
 	/* remove /dev/fb* */
 	unregister_framebuffer(mfd->fbi);
@@ -1616,9 +1614,7 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 	init_completion(&mfd->commit_comp);
 	mutex_init(&mfd->sync_mutex);
 	mutex_init(&mfd->queue_mutex);
-	init_waitqueue_head(&mfd->commit_queue);
-	mfd->commit_thread = kthread_run(msm_fb_commit_thread, mfd,
-			"msmfb_commit_thread");
+	INIT_WORK(&mfd->commit_work, msm_fb_commit_wq_handler);
 	mfd->msm_fb_backup = kzalloc(sizeof(struct msm_fb_backup_type),
 		GFP_KERNEL);
 	if (mfd->msm_fb_backup == 0) {
@@ -2128,8 +2124,7 @@ static int msm_fb_pan_display_ex(struct fb_info *info,
 	mfd->is_committing = 1;
 	INIT_COMPLETION(mfd->commit_comp);
 	atomic_inc(&mfd->commit_cnt);
-	mfd->wake_commit_thread = 1;
-	wake_up_interruptible_all(&mfd->commit_queue);
+	schedule_work(&mfd->commit_work);
 	mutex_unlock(&mfd->sync_mutex);
 	if (wait_for_finish)
 		msm_fb_pan_idle(mfd);
@@ -2299,38 +2294,28 @@ void msm_fb_release_busy(struct msm_fb_data_type *mfd)
 	complete_all(&mfd->commit_comp);
 	mutex_unlock(&mfd->sync_mutex);
 }
-static int msm_fb_commit_thread(void *data)
+static void msm_fb_commit_wq_handler(struct work_struct *work)
 {
-	int ret = 0;
-	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *) data;
+	struct msm_fb_data_type *mfd;
 	struct fb_var_screeninfo *var;
 	struct fb_info *info;
 	struct msm_fb_backup_type *fb_backup;
 	u32 overlay_commit = false;
 
-	while (!kthread_should_stop()) {
-		ret = wait_event_interruptible(mfd->commit_queue,
-				mfd->wake_commit_thread);
-		if (ret >= 0) {
-			mfd->wake_commit_thread = 0;
-			mutex_lock(&mfd->queue_mutex);
-			while (atomic_read(&mfd->commit_cnt) > 0) {
-				fb_backup = (struct msm_fb_backup_type *)
-					mfd->msm_fb_backup;
-				info = &fb_backup->info;
-				if (fb_backup->disp_commit.flags &
-						MDP_DISPLAY_COMMIT_OVERLAY) {
-					overlay_commit = true;
-					mdp4_overlay_commit(info);
-				} else {
-					var = &fb_backup->disp_commit.var;
-					msm_fb_pan_display_sub(var, info);
-					msm_fb_release_busy(mfd);
-				}
-			}
-			if (overlay_commit)
-				mdp4_overlay_commit_finish(info);
-			mutex_unlock(&mfd->queue_mutex);
+	mfd = container_of(work, struct msm_fb_data_type, commit_work);
+	mutex_lock(&mfd->queue_mutex);
+
+	while (atomic_read(&mfd->commit_cnt) > 0) {
+		fb_backup = (struct msm_fb_backup_type *)mfd->msm_fb_backup;
+		info = &fb_backup->info;
+		if (fb_backup->disp_commit.flags &
+			MDP_DISPLAY_COMMIT_OVERLAY) {
+				overlay_commit = true;
+				mdp4_overlay_commit(info);
+		} else {
+			var = &fb_backup->disp_commit.var;
+			msm_fb_pan_display_sub(var, info);
+			msm_fb_release_busy(mfd);
 		}
 	}
 
@@ -2340,7 +2325,9 @@ static int msm_fb_commit_thread(void *data)
 					backlight_duration);
 #endif
 
-	return 0;
+	if (overlay_commit)
+		mdp4_overlay_commit_finish(info);
+	mutex_unlock(&mfd->queue_mutex);
 }
 
 static int msm_fb_check_var(struct fb_var_screeninfo *var, struct fb_info *info)
