b8409a607443127ef9866749f239b05066d8f5b6
Azhar Shaikh <azhars@codeaurora.org>
2014-10-17 16:27:59 -0700

    PM: Fix a bug in dpm_suspend_late()
    If there is any error while executing "late suspend" callbacks, then
    the pm_runtime_enable() will not be called.
    Hence in case of an error call pm_runtime_enable() before exiting from
    dpm_suspend_late().
    
    CRs-fixed: 740511
    Change-Id: Iedacbbf47a4ef0ab0a0ace71fc809d056d56f89f
    Signed-off-by: Azhar Shaikh <azhars@codeaurora.org>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -949,6 +949,7 @@ static int device_suspend_late(struct device *dev, pm_message_t state)
 {
 	pm_callback_t callback = NULL;
 	char *info = NULL;
+	int error = 0;
 
 	if (dev->pm_domain) {
 		info = "late power domain ";
@@ -969,7 +970,15 @@ static int device_suspend_late(struct device *dev, pm_message_t state)
 		callback = pm_late_early_op(dev->driver->pm, state);
 	}
 
-	return dpm_run_callback(callback, dev, state, info);
+	error = dpm_run_callback(callback, dev, state, info);
+	if (error)
+		/*
+		 * dpm_resume_early wouldn't be run for this failed device,
+		 * hence enable runtime_pm now
+		 */
+		pm_runtime_enable(dev);
+
+	return error;
 }
 
 /**
