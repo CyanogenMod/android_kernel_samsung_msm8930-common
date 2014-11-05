753c4e4b0ea949ae676050edcca004b4011f34dd
Arne Coucheron <arco68@gmail.com>
2014-11-06 00:36:21 +0100

    mmc: Re-enable some features
    Change-Id: I31da71a18adf3f8f9f2002014e2327153771a88e

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1569,9 +1569,8 @@ static int mmc_suspend(struct mmc_host *host)
 
 	if (mmc_can_poweroff_notify(host->card))
 		err = mmc_poweroff_notify(host->card, EXT_CSD_POWER_OFF_SHORT);
-	/* Disable CMD5 because of init fail on eMMC */
-	//else if (mmc_card_can_sleep(host))
-	//	err = mmc_card_sleep(host);
+	else if (mmc_card_can_sleep(host))
+		err = mmc_card_sleep(host);
 	else if (!mmc_host_is_spi(host))
 		mmc_deselect_cards(host);
 	host->card->state &= ~(MMC_STATE_HIGHSPEED | MMC_STATE_HIGHSPEED_200);
--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -5945,9 +5945,8 @@ msmsdcc_probe(struct platform_device *pdev)
 	mmc->caps2 |= plat->packed_write;
 
 	mmc->caps2 |= (MMC_CAP2_BOOTPART_NOACC | MMC_CAP2_DETECT_ON_ERR);
-	/* Disable Sanitize & BKOPS */
-	//mmc->caps2 |= MMC_CAP2_SANITIZE;
-	//mmc->caps2 |= MMC_CAP2_INIT_BKOPS;
+	mmc->caps2 |= MMC_CAP2_SANITIZE;
+	mmc->caps2 |= MMC_CAP2_INIT_BKOPS;
 	mmc->caps2 |= MMC_CAP2_POWEROFF_NOTIFY;
 	mmc->caps2 |= MMC_CAP2_STOP_REQUEST;
 	mmc->caps2 |= MMC_CAP2_ASYNC_SDIO_IRQ_4BIT_MODE;
@@ -6561,8 +6560,7 @@ static int msmsdcc_runtime_idle(struct device *dev)
 		return 0;
 
 	/* Idle timeout is not configurable for now */
-	/* Disable Runtime PM because of potential issues */
-	//pm_schedule_suspend(dev, host->idle_tout_ms);
+	pm_schedule_suspend(dev, host->idle_tout_ms);
 
 	return -EAGAIN;
 }
