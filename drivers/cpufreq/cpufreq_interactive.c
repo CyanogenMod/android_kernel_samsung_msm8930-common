36bc183d3aeeb6d1c73180a0ef23969f439dcc07
rahul.khandelwal <rahul.khandelwal@spreadtrum.com>
2015-04-17 11:45:23 +0530

    subsystem: CPU FREQUENCY DRIVERS- Set cpu_load calculation on current frequency
    In timer, cpu_load is calcuated on target_freq.
    cpu_load = loadadjfreq / pcpu->target_freq;
    But cpu is actually running on current freq i.e. pcpu->policy->cur. So cpu_load
    should be calculated on current frequency.
    cpu_load = loadadjfreq / pcpu->policy->cur;
    
    Change-Id: I89db6b68e9f82aa52077f6bf7d819dab74265790
    Signed-off-by: rahul.khandelwal <rahul.khandelwal@spreadtrum.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -383,7 +383,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 	spin_lock_irqsave(&pcpu->target_freq_lock, flags);
 	do_div(cputime_speedadj, delta_time);
 	loadadjfreq = (unsigned int)cputime_speedadj * 100;
-	cpu_load = loadadjfreq / pcpu->target_freq;
+	cpu_load = loadadjfreq / pcpu->policy->cur;
 	boosted = boost_val || now < boostpulse_endtime;
 	this_hispeed_freq = max(hispeed_freq, pcpu->policy->min);
 
