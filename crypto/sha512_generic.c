848d3630088e6dfd7416ec717ba64d3e65caa7bc
Arne Coucheron <arco68@gmail.com>
2015-05-10 03:24:37 +0200

    Squashed revert of a bunch of stuff
     * Suspecting one of these are causing random reboots,
       but don't have time right now to pin-point which one,
       so do a whole-sale revert.
    Revert "msm8930: Add msm-pcm-loopback device"
    
    This reverts commit 5bdade027202272bf202cc091ed794d4d735186d.
    
    Revert "defconfig: Enable optimized SHA256 and SHA512"
    
    This reverts commit e7a70c9a8b5b0e5e8b7dc2b4a14855e84aa0e0aa.
    
    Revert "arm: crypto: Fix compile errors for 3.4 kernel"
    
    This reverts commit 8c6388f1b4fc1170cd7a76d8ec93c81784578219.
    
    Revert "arm: crypto: Add optimized SHA-256/224"
    
    This reverts commit 7c97fc234f418e176caaea4e11e399a0f3369e40.
    
    Revert "crypto: sha512 - Expose generic sha512 routine to be callable from other modules"
    
    This reverts commit d2e97ac741fa87e004f49086c80438353a71697a.
    
    Revert "crypto: sha256 - Expose SHA256 generic routine to be callable externally."
    
    This reverts commit ae9a729d75efd7c3ecb3498e150f73d69d6196b5.
    
    Revert "crypto: add crypto_[un]register_shashes for [un]registering multiple shash entries at once"
    
    This reverts commit f5f247c164a2348ee8962dad0b54f9a32cb56991.
    
    Revert "kernel: Restrict permissions of /proc/iomem."
    
    This reverts commit 80bf84316ddf5da782fce84dbde7422314b0ddf9.
    
    Revert "scripts/dtc/libfdt: add integer overflow checks"
    
    This reverts commit f0ba52530d82ae9330a7aa7eabc262a6d5aa208e.
    
    Revert "usb: dwc3: gadget: Protect against ep disabling during completion"
    
    This reverts commit 9583d4e36e8c4aa654089fb6fd5f360b3ef022cc.
    
    Revert "mm: Increase number of GFP masks"
    
    This reverts commit 3ff4e44bb483701f338f7f4d655e86d000aa3288.
    
    Revert "arm: lib: Fix makefile bug"
    
    This reverts commit 14f3b3b52e9e799d43b5061f7ac44197020b71cc.
    
    Revert "msm: memutils: memcpy, memmove, copy_page optimization"
    
    This reverts commit ca9da1aadca645dad839df1b4dfb9b1e5e69d268.
    
    Revert "ARM: 7449/1: use generic strnlen_user and strncpy_from_user functions"
    
    This reverts commit dcfe4c459e8e0dcde3675d5cd078e51620a72932.
    
    Revert "proc: make oom adjustment files user read-only"
    
    This reverts commit 783442ae2dcf571c00de97858422e75e2cdb0a8e.
    
    Revert "Revert "Grants system server access to /proc/<pid>/oom_adj for Android applications.""
    
    This reverts commit ba899bbc1b8315d4ed54f44694c43a0265b63d1f.
    
    Revert "fs/seq_file: Use vmalloc by default for allocations > PAGE_SIZE"
    
    This reverts commit 2a2528d23358c9fa46e380fbfd5757020e0c2752.
    
    Revert "fs/seq_file: fallback to vmalloc allocation"
    
    This reverts commit f1af4de54d691d1e6c97e8b8ff75b0438b8bed26.
    
    Revert "seq_file: always clear m->count when we free m->buf"
    
    This reverts commit d8cf113e02ec9278db9a965c6ed7f01a78e62622.
    
    Revert "Revert "seq_file: Allocate memory using vmalloc when kmalloc failed""
    
    This reverts commit 481cf9c5412cef4166d684ff7adb88040de53895.
    
    Revert "seq_file: Remove Samsung changes"
    
    This reverts commit f084564e8227bce3845e8499980213687cf7dc0f.
    
    Revert "lmk: Sync with hammerhead"
    
    This reverts commit 65d998eefa82da0d1d7fe2cd3602995ef4e8a630.
    
    Revert "mm, oom: make dump_tasks public"
    
    This reverts commit eebdd88b9a40e8bac1605fa27c806b8b75811d33.
    
    Revert "nick kvfree() from apparmor"
    
    This reverts commit 504f9ae571df757a72ea245eaf27de7c10de724b.
    
    Revert "Revert "lmk: Sync with LA.AF""
    
    This reverts commit 89ba3e60cffce9176dbf1d32f6d7a0a52ae006e8.

--- crypto/sha512_generic.c
+++ crypto/sha512_generic.c
@@ -163,8 +163,8 @@ sha384_init(struct shash_desc *desc)
 	return 0;
 }
 
-int crypto_sha512_update(struct shash_desc *desc, const u8 *data,
-			unsigned int len)
+static int
+sha512_update(struct shash_desc *desc, const u8 *data, unsigned int len)
 {
 	struct sha512_state *sctx = shash_desc_ctx(desc);
 
@@ -197,7 +197,6 @@ int crypto_sha512_update(struct shash_desc *desc, const u8 *data,
 
 	return 0;
 }
-EXPORT_SYMBOL(crypto_sha512_update);
 
 static int
 sha512_final(struct shash_desc *desc, u8 *hash)
@@ -216,10 +215,10 @@ sha512_final(struct shash_desc *desc, u8 *hash)
 	/* Pad out to 112 mod 128. */
 	index = sctx->count[0] & 0x7f;
 	pad_len = (index < 112) ? (112 - index) : ((128+112) - index);
-	crypto_sha512_update(desc, padding, pad_len);
+	sha512_update(desc, padding, pad_len);
 
 	/* Append length (before padding) */
-	crypto_sha512_update(desc, (const u8 *)bits, sizeof(bits));
+	sha512_update(desc, (const u8 *)bits, sizeof(bits));
 
 	/* Store state in digest */
 	for (i = 0; i < 8; i++)
@@ -246,7 +245,7 @@ static int sha384_final(struct shash_desc *desc, u8 *hash)
 static struct shash_alg sha512 = {
 	.digestsize	=	SHA512_DIGEST_SIZE,
 	.init		=	sha512_init,
-	.update		=	crypto_sha512_update,
+	.update		=	sha512_update,
 	.final		=	sha512_final,
 	.descsize	=	sizeof(struct sha512_state),
 	.base		=	{
@@ -260,7 +259,7 @@ static struct shash_alg sha512 = {
 static struct shash_alg sha384 = {
 	.digestsize	=	SHA384_DIGEST_SIZE,
 	.init		=	sha384_init,
-	.update		=	crypto_sha512_update,
+	.update		=	sha512_update,
 	.final		=	sha384_final,
 	.descsize	=	sizeof(struct sha512_state),
 	.base		=	{
