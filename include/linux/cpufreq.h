84269c5ecffb2f5e04ae7c76bf4f60c456b63b5f
alexax66 <alexax66@gmail.com>
2015-11-18 19:15:52 +0000

    cpufreq: Add Alucard CPU Governor
    Alucard is based on Ondemand but has been heavily tweaked to bring better battery
    life and performance. It has been known to be battery friendly without sacrificing
    much performance.
    
     Conflicts:
    	arch/arm/configs/cyanogen_serrano_defconfig
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -379,6 +379,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
 #endif
 
 
