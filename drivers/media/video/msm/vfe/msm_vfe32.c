dd37263f5276078392de676f872fcf1bc3ed81f2
Arne Coucheron <arco68@gmail.com>
2016-04-22 02:51:52 +0200

    msm: camera: Fix compile error in msm_vfe32.c after previous commit
    Change-Id: I74441df7fe1dca44302fbcea94b2e665e9918791

--- drivers/media/video/msm/vfe/msm_vfe32.c
+++ drivers/media/video/msm/vfe/msm_vfe32.c
@@ -6166,7 +6166,7 @@ static const struct v4l2_subdev_ops msm_vfe_subdev_ops = {
 
 #if defined(VFE_IOMMU_FAULT_HANDLER)
 static int vfe_iommu_fault_handler(struct iommu_domain *domain,
-		struct device *dev, unsigned long iova, int flags)
+		struct device *dev, unsigned long iova, int flags, void *token)
 {
     pr_err("iommu page fault has happened\n");
     atomic_set(&fault_recovery, 1);
@@ -6250,7 +6250,7 @@ int msm_axi_subdev_init(struct v4l2_subdev *sd,
 
 #if defined(VFE_IOMMU_FAULT_HANDLER)
 	iommu_set_fault_handler(mctl->domain,
-						 vfe_iommu_fault_handler);
+		vfe_iommu_fault_handler, NULL);
 #endif
 
 #if defined(VFE_RESET_HANDLER)
