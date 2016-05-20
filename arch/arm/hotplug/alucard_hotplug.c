5f089b36fee6cf71ba585d1438498b64253f5559
tioguda <guda.flavio@gmail.com>
2016-05-20 20:27:26 -0300

    alucard_hotplug:move to the hotplug directory

--- arch/arm/hotplug/alucard_hotplug.c
+++ arch/arm/hotplug/alucard_hotplug.c
@@ -22,7 +22,7 @@
 #include <linux/mutex.h>
 #include <linux/module.h>
 #include <linux/slab.h>
-#include "acpuclock.h"
+#include "../mach-msm/acpuclock.h"
 
 #if defined(CONFIG_POWERSUSPEND)
 #include <linux/powersuspend.h>
